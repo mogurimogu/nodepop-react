5f84bc3fce037a6ed551b80d51d083a5b38a5a39
Luis <40238711+mogurimogu@users.noreply.github.com>
2021-10-30 17:03:05 +0200

    Nodepop Rreact APP
    Empezado proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/index.scss";
+import AuthPage from "./components/auth/index";
+
+function App() {
+  return (
+    <div className="App">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
